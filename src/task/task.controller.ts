149d2410ba5e0984a20bcd888ca850378c9f5d51
ArtemZayarny <artyom.zayarny@gmail.com>
2025-06-22 17:26:03 +0300

    small fixes

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,43 +1,52 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param,  Patch,  Post, UseGuards, } from '@nestjs/common';
-import { TaskDto } from './dto/task.dto';
-import { TaskService } from './task.service';
-import { TASK_NOT_FOUND } from './constants';
-import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from "@nestjs/common";
+import { TaskDto } from "./dto/task.dto";
+import { TaskService } from "./task.service";
+import { TASK_NOT_FOUND } from "./constants";
+import { JwtAuthGuard } from "src/auth/guard/jwt.guard";
 
-@Controller('task')
+@Controller("task")
 export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
 
-    constructor(private readonly taskService:TaskService){}
+  @UseGuards(JwtAuthGuard)
+  @Post("create")
+  async createTask(@Body() task: TaskDto) {
+    return this.taskService.create(task);
+  }
 
-   @UseGuards(JwtAuthGuard)
-    @Post('create')
-    async createTasktask(@Body() task:TaskDto) {
-       return this.taskService.create(task)
+  @UseGuards(JwtAuthGuard)
+  @Get(":id")
+  async fetchTaskListByUserId(@Param("id") id: string) {
+    return this.taskService.getTasksByUserId(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(":id")
+  async deleteTask(@Param("id") id: string) {
+    const deletedProduct = this.taskService.deleteTask(id);
+    if (!deletedProduct) {
+      throw new NotFoundException(TASK_NOT_FOUND);
+    }
+  }
+
+  @Patch(":id")
+  async updateTask(@Param("id") id: string, @Body() task: TaskDto) {
+    const updatedProduct = await this.taskService.updateById(id, task);
+
+    if (!updatedProduct) {
+      throw new NotFoundException(TASK_NOT_FOUND);
     }
 
-    @UseGuards(JwtAuthGuard)
-    @Get(':id')
-    async fetchTaskListByUserId(@Param('id') id:string) {
-        return this.taskService.getTasksByUserId(id)
-    }
-
-    @UseGuards(JwtAuthGuard)
-    @Delete(':id')
-    async deleteTask(@Param('id') id:string){
-        const deletedProduct = this.taskService.deleteTask(id)
-        if(!deletedProduct) {
-            throw new NotFoundException(TASK_NOT_FOUND);
-        }
-    }
-
-    @Patch(':id')
-    async updateTask(@Param('id') id:string, @Body() task:TaskDto){
-        const updatedProduct = await this.taskService.updateById(id, task);
-
-        if (!updatedProduct) {
-          throw new NotFoundException(TASK_NOT_FOUND);
-        }
-    
-        return updatedProduct;
-    }
+    return updatedProduct;
+  }
 }
